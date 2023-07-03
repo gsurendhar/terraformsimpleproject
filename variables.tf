6480e4e4e064482ebba25614f9ac43b820ffd867
gsurendhar <surendharg123@gmail.com>
2023-07-03 10:43:59 +0530

    variable file is added

---
+++ variables.tf
@@ -0,0 +1,11 @@
+variable "region" {
+  description = "all services are created in this region"
+  type        = "string"
+  default     = "us-east-1"
+}
+
+variable "profile" {
+  description = "who wants to create services"
+  type        = "string"
+  default     = "Surendhar"
+}
