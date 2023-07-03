e6e0cb4f061b7947bd9b7eb5ef9dae98414fc7c8
gsurendhar <surendharg123@gmail.com>
2023-07-03 19:41:39 +0530

    merge

--- alb.tf
+++ alb.tf
@@ -41,7 +41,7 @@ resource "aws_lb_target_group" "LB-taget" {
 # load balancer listeners
 resource "aws_lb_listener" "forward" {
   load_balancer_arn = aws_lb.LB.arn
-  port              = 80
+  port              = 81
   protocol          = "HTTP"
   default_action {
     type             = "forward"
