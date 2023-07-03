804bd14ad4e4cb9316aff8f5e3f13a74c5396134
gsurendhar <surendharg123@gmail.com>
2023-07-03 15:40:03 +0000

    instance not working with security group

--- instance.tf
+++ instance.tf
@@ -1,9 +1,9 @@
 resource "aws_instance" "MYEC2" {
   ami                    = "ami-053b0d53c279acc90"
   instance_type          = "t2.micro"
-  vpc_security_group_ids = ["$(aws_security_group.MYSG.id)"]
-  # security_groups = aws_security_group.surisg.id
-  # vpc_id    = aws_vpc.VPC.id
+  # vpc_security_group_ids = ["$(aws_security_group.MYSG.id)"]
+ # security_groups =  ["$(sg-0f5fb6cdfbd4c71e4)", "$(sg-0f732385c3b6e9058)"]
+# vpc_id    = aws_vpc.VPC.id
   subnet_id = aws_subnet.subnet-1.id
   key_name  = "linux"
 
