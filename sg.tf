e9105ae57aaa30832c3671cc5774a61d7fd1ba44
gsurendhar <surendharg123@gmail.com>
2023-07-03 15:08:22 +0530

    security group added

---
+++ sg.tf
@@ -0,0 +1,21 @@
+# security group
+resource "aws_security_group" "MYSG" {
+  vpc_id = aws_vpc.VPC.id
+  ingress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  tags = {
+    Name = "MYSG-1"
+  }
+
+
+}
